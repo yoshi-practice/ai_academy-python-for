ee5cbaec4459266f628b695b6487fc2ff2e84a4d
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-07 23:22:40 +0900

    Add test answer

---
+++ test.py
@@ -0,0 +1,13 @@
+# --- 1 ---
+num = 3
+while num > 0:
+    print(num)
+    num = num-1
+print("GO!")
+
+# --- 2 ---
+list = []
+for x in range(1, 101):
+    if x % 2 == 0:
+        list.append(x)
+print(list)
